572862a3b6a63cf57adff7e7b24f8d0eb92c0f24
Olivier Giniaux <oginiaux@gmail.com>
2020-01-31 03:30:31 +0100

    Fix editor-only icons in Resources directory

--- Assets/Plugins/ProjectCurator/Editor/ProjectIcons.cs
+++ Assets/Plugins/ProjectCurator/Editor/ProjectIcons.cs
@@ -1,21 +1,19 @@
-﻿#pragma warning disable 649
-
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace Nanolabo
 {
-    public class ProjectIcons : ProjectReferences<ProjectIcons>
+    public static class ProjectIcons
     {
-        [SerializeField]
-        private Texture2D linkBlack;
-        public static Texture2D LinkBlack => Instance.linkBlack;
+        private const string PLUGIN_LOCATION = "Assets/Plugins/ProjectCurator/";
 
-        [SerializeField]
-        private Texture2D linkWhite;
-        public static Texture2D LinkWhite => Instance.linkWhite;
+        private static Texture2D linkBlack;
+        public static Texture2D LinkBlack => linkBlack ?? (linkBlack = (Texture2D)AssetDatabase.LoadAssetAtPath(PLUGIN_LOCATION + "Images/link_black.png", typeof(Texture2D)));
 
-        [SerializeField]
-        private Texture2D linkBlue;
-        public static Texture2D LinkBlue => Instance.linkBlue;
+        private static Texture2D linkWhite;
+        public static Texture2D LinkWhite => linkWhite ?? (linkWhite = (Texture2D)AssetDatabase.LoadAssetAtPath(PLUGIN_LOCATION + "Images/link_white.png", typeof(Texture2D)));
+
+        private static Texture2D linkBlue;
+        public static Texture2D LinkBlue => linkBlue ?? (linkBlue = (Texture2D)AssetDatabase.LoadAssetAtPath(PLUGIN_LOCATION + "Images/link_blue.png", typeof(Texture2D)));
     }
 }
