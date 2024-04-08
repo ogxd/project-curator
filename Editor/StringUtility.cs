f555ce92d4a1bc79d52dc23475cf0ecd253b502b
Rhys van der Waerden <rhys.vdw@gmail.com>
2024-04-08 10:45:15 +1000

    Whitespace changes
    Co-authored-by: Olivier Giniaux <oginiaux@gmail.com>

--- Editor/StringUtility.cs
+++ Editor/StringUtility.cs
@@ -1,18 +1,18 @@
 using System;
 
 namespace Ogxd.ProjectCurator {
-  internal static class StringUtility
-  {
-    // https://stackoverflow.com/a/7170953/317135
-    public static string TrimEnd(
-      this string input,
-      string suffixToRemove,
-      StringComparison comparisonType = StringComparison.CurrentCulture
-    ) {
-      if (suffixToRemove != null && input.EndsWith(suffixToRemove, comparisonType)) {
-        return input[0..^suffixToRemove.Length];
-      }
-      return input;
+    internal static class StringUtility
+    {
+        // https://stackoverflow.com/a/7170953/317135
+        public static string TrimEnd(
+            this string input,
+            string suffixToRemove,
+            StringComparison comparisonType = StringComparison.CurrentCulture) 
+        {
+            if (suffixToRemove != null && input.EndsWith(suffixToRemove, comparisonType)) {
+                return input[0..^suffixToRemove.Length];
+            }
+            return input;
+        }
     }
-  }
 }
