da8a264a30daf02a7365dbac920203fe1c0d9277
Rhys van der Waerden <rhys.vdw@gmail.com>
2024-04-09 17:31:31 +1000

    Fix warning when deleting empty folders (#23)
    * Tidy AssetProccessor
    
    - Move private field up
    - Correct formatting for private field
    - Prefer to use readonly
    
    * Add handling for adding new folders to database
    
    * Whitespace changes
    
    Co-authored-by: Olivier Giniaux <oginiaux@gmail.com>
    
    ---------
    
    Co-authored-by: Olivier Giniaux <oginiaux@gmail.com>

--- Editor/AssetProcessor.cs
+++ Editor/AssetProcessor.cs
@@ -9,6 +9,8 @@ namespace Ogxd.ProjectCurator
     /// </summary>
     public class AssetProcessor : UnityEditor.AssetModificationProcessor
     {
+        private static readonly Queue<Action> _actions = new Queue<Action>();
+
         [InitializeOnLoadMethod]
         public static void Init()
         {
@@ -20,20 +22,18 @@ namespace Ogxd.ProjectCurator
         /// </summary>
         private static void OnUpdate()
         {
-            if (Actions.Count > 0) {
-                while (Actions.Count > 0) {
-                    Actions.Dequeue()?.Invoke();
+            if (_actions.Count > 0) {
+                while (_actions.Count > 0) {
+                    _actions.Dequeue()?.Invoke();
                 }
                 ProjectCurator.SaveDatabase();
             }
         }
 
-        private static Queue<Action> Actions = new Queue<Action>();
-
         static string[] OnWillSaveAssets(string[] paths)
         {
             if (ProjectCuratorData.IsUpToDate) {
-                Actions.Enqueue(() => {
+                _actions.Enqueue(() => {
                     foreach (string path in paths) {
                         var guid = AssetDatabase.AssetPathToGUID(path);
                         var removedAsset = ProjectCurator.RemoveAssetFromDatabase(guid);
@@ -46,8 +46,16 @@ namespace Ogxd.ProjectCurator
 
         static void OnWillCreateAsset(string assetPath)
         {
+            // Due to an apparent bug in Unity, newly created folders give the
+            // path of the meta file, instead of the folder itself. Trim the
+            // .meta off the end in the case that this happens.
+            //
+            // .meta files are not assets, so there are no other cases where
+            // this will have any effect.
+            assetPath = assetPath.TrimEnd(".meta");
+
             if (ProjectCuratorData.IsUpToDate) {
-                Actions.Enqueue(() => {
+                _actions.Enqueue(() => {
                     var guid = AssetDatabase.AssetPathToGUID(assetPath);
                     if (guid != string.Empty) {
                         ProjectCurator.AddAssetToDatabase(guid);
